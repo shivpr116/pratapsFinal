52da38955c387d25b24ac56f0ccc5d82572ebe59
shivpr116 <52942124+shivpr116@users.noreply.github.com>
2019-08-12 12:38:44 -0400

    Add files via upload

---
+++ assets/data/bottleData.ts
@@ -0,0 +1,10 @@
+import {bottle} from '../../app/Bottle';
+
+export const inform: bottle[] = [
+
+    {style:'classy', color:'blue', type:'plastic'},
+    {style:'old', color:'red', type:'clay'},
+    {style:'traditional', color:'black', type:'matte'}
+]
+   
+
